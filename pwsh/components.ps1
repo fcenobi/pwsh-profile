c41357131c560a73c037c34e1c1d6156620e7f2d
Filippo Cenobi <11414315+fcenobi@users.noreply.github.com>
2020-09-05 20:34:32 +0200

    Update components.ps1

--- pwsh/components.ps1
+++ pwsh/components.ps1
@@ -11,13 +11,16 @@
 Push-Location (Join-Path (Split-Path -parent $profile) "components")
 
 # From within the ./components directory...
+. .\general-modules.ps1
 # . .\cargo.ps1
-# . .\dotnet.ps1
+. .\dotnet.ps1
 . .\choco.ps1
 . .\git.ps1
 . .\npm.ps1
 . .\scoop.ps1
 . .\yarn.ps1
+. .\admintolboox.ps1
+
 
 Pop-Location
 
