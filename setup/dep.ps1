812ed308ebf797a7927fd44e415ac1610a4f22ac
Filippo Cenobi <11414315+fcenobi@users.noreply.github.com>
2020-09-05 20:22:44 +0200

    Update dep.ps1

--- setup/dep.ps1
+++ setup/dep.ps1
@@ -11,6 +11,7 @@ install-Module -Name posh-git             -Scope AllUsers -AllowClobber -Force -
 install-Module -Name npm-completion       -Scope AllUsers -AllowClobber -Force -AcceptLicense -ErrorAction Continue
 install-Module -Name yarn-completion      -Scope AllUsers -AllowClobber -Force -AcceptLicense -ErrorAction Continue
 install-Module -Name scoop-completion -Scope AllUsers -AllowClobber -Force -AcceptLicense -ErrorAction Continue
+install-module -name admintoolbox -Scope AllUsers -AllowClobber -Force -AcceptLicense -ErrorAction Continue
 import-Module -Name scoop-completion           -Global -FORCE -ErrorAction Continue
 import-Module -Name posh-cargo           -Global -FORCE -ErrorAction Continue
 import-Module -Name PSReadLine           -Global -FORCE -ErrorAction Continue
@@ -21,7 +22,7 @@ import-Module -Name posh-dotnet          -Global -FORCE -ErrorAction Continue
 import-Module -Name posh-git             -Global -FORCE -ErrorAction Continue
 import-Module -Name npm-completion       -Global -FORCE -ErrorAction Continue
 import-Module -Name yarn-completion      -Global -FORCE -ErrorAction Continue
-
+import-module -name admintoolbox       -Global -FORCE -ErrorAction Continue
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 choco feature enable -n allowGlobalConfirmation
