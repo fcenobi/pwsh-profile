6d34861dcc81a1de37ae49acb4cd458a9ae84f8f
Filippo Cenobi <11414315+fcenobi@users.noreply.github.com>
2020-09-08 06:52:05 +0200

    Update scoop.ps1

--- pwsh/components/scoop.ps1
+++ pwsh/components/scoop.ps1
@@ -2,12 +2,16 @@
 # components/scoop.ps1 #
 # -------------------- #
 
-# Write-Output "components/scoop.ps1"
+Write-Output "components/scoop.ps1"
 
-try {
-  Import-Module -ErrorAction Stop "$($(Get-Item $(Get-Command -ErrorAction Stop scoop).Path).Directory.Parent.FullName)\modules\scoop-completion"
-} catch {
-  Write-Output "Error Importing Scoop Completion Module"
+#try {
+#  Import-Module -ErrorAction Stop "$($(Get-Item $(Get-Command -ErrorAction Stop scoop).Path).Directory.Parent.FullName)\modules\scoop-completion"
+#} catch {
+#  Write-Output "Error Importing Scoop Completion Module"
+#}
+
+if ((Get-Command tabexpansion -ErrorAction SilentlyContinue) -and (Get-Module -ListAvailable scop-completion -ErrorAction SilentlyContinue)) {
+  Import-Module -Name scoop-completion
 }
 
 # EOF #
