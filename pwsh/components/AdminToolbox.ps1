c37eb895189423a9b60679511487b7c66ae61269
Filippo Cenobi <11414315+fcenobi@users.noreply.github.com>
2020-09-11 11:47:44 +0200

    Update AdminToolbox.ps1

--- pwsh/components/AdminToolbox.ps1
+++ pwsh/components/AdminToolbox.ps1
@@ -2,7 +2,7 @@
 # components/AdminToolbox.ps1 #
 # ------------------- #
 
-Write-Output "components/AdminToolbox.ps1"
+#Write-Output "components/AdminToolbox.ps1"
 
 if  (Get-Module -ListAvailable -name AdminToolbox  -ErrorAction SilentlyContinue )  { Import-Module -Name AdminToolbox  -Force  -ErrorAction SilentlyContinue }
   
